aa7f9512805d3420260f32b461567846c63889d5
Sergey Rudenko <hzzh@outlook.com>
2018-12-10 11:05:54 +0200

    lags fixed

--- WindowsFormsCamers/Form1.cs
+++ WindowsFormsCamers/Form1.cs
@@ -2,20 +2,18 @@
 using System.Drawing;
 using System.Windows.Forms;
 using AForge.Video;
-using System.Drawing.Imaging;
 using AForge.Imaging.Filters;
 using AForge.Imaging;
+using System.Drawing.Drawing2D;
 
 namespace WindowsFormsCamers
 {
     public partial class Form1 : Form
     {
-
         public Form1()
         {
             InitializeComponent();
         }
-
         MJPEGStream stream;
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -27,21 +25,26 @@ namespace WindowsFormsCamers
         Bitmap createBitmap(object sender, NewFrameEventArgs eventArgs)
         {
             Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
-            return new Bitmap(bmp, new Size(SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height));
+            bmp = new Bitmap(bmp, new Size(SystemInformation.VirtualScreen.Width/7, SystemInformation.VirtualScreen.Height/7));
+            using (bmp)
+            {
+                var bmp2 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                using (var g = Graphics.FromImage(bmp2))
+                {
+                    g.InterpolationMode = InterpolationMode.NearestNeighbor;
+                    g.DrawImage(bmp, new Rectangle(Point.Empty, bmp2.Size));
+                    return bmp2;
+                }
+            }
         }
 
-        void MoveMouse(int X, int Y)
-        {
-            this.Cursor = new Cursor(Cursor.Current.Handle);
-            Cursor.Position = new System.Drawing.Point(X, Y);
-            Cursor.Clip = new Rectangle(this.Location, this.Size);
-        }
+
 
         void stream_NewFrame(object sender, NewFrameEventArgs eventArgs)
         {
             //создание стрима на весь экран
             Bitmap bmp = createBitmap(sender, eventArgs);
-
+           
             //выделение желтого цвета
             EuclideanColorFiltering filter = new EuclideanColorFiltering();
             filter.CenterColor = new RGB(220, 220, 0); //желтый цвет
@@ -49,44 +52,39 @@ namespace WindowsFormsCamers
             filter.ApplyInPlace(bmp);
             bmp.RotateFlip(RotateFlipType.RotateNoneFlipX); //зеркально
 
-            //сделать серым для дальнейшей работы
-            BitmapData objectsData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, bmp.PixelFormat);
-            Grayscale grayscaleFilter = new Grayscale(0.2125, 0.7154, 0.0721);
-            UnmanagedImage grayImage = grayscaleFilter.Apply(new UnmanagedImage(objectsData));
-            bmp.UnlockBits(objectsData);
             BlobCounter blobCounter = new BlobCounter();
-            blobCounter.ProcessImage(grayImage);
-
             //получение координат
-            blobCounter.MinWidth = 50;
-            blobCounter.MinHeight = 50;
+            blobCounter.MinWidth = 15;
+            blobCounter.MinHeight = 15;
             blobCounter.FilterBlobs = true;
-            blobCounter.ProcessImage(grayImage);
+            blobCounter.ProcessImage(bmp);
             Rectangle[] rects = blobCounter.GetObjectsRectangles();
             foreach (Rectangle recs in rects)
             {
-                //TODO
-                MoveMouse(recs.X, recs.Y);
-
-                /*
                 if (rects.Length > 0) //обводить в квадарат
                 {
-                    Rectangle objectRect = rects[0];
-                    Graphics g = Graphics.FromImage(bmp);
-                    using (Pen pen = new Pen(Color.FromArgb(160, 255, 160), 5))
-                    {
-                        g.DrawRectangle(pen, objectRect);
-                    }
-                    g.Dispose();
+                    this.Cursor = new Cursor(Cursor.Current.Handle);
+                    Cursor.Position = new System.Drawing.Point(recs.X, recs.Y);
+                    Cursor.Clip = new Rectangle(this.Location, this.Size);
+
+                    //Rectangle objectRect = rects[0];
+                    //Graphics g = Graphics.FromImage(bmp);
+                    //using (Pen pen = new Pen(Color.FromArgb(160, 255, 160), 5))
+                    //{
+                    //   g.DrawRectangle(pen, objectRect);
+                    //}
+                   //g.Dispose();
                 }
-                */
+               
+               
+               
             }
-            Application.Exit();
+            //pictureBox1.Image = bmp; //рисовать
         }
 
         private void Form1_Shown(object sender, EventArgs e)
         {
-            this.Hide();
+            Application.Exit();
         }
     }
 }
