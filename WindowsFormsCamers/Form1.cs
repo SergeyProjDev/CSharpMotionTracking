7fd841dbca07a66647cae1625e68708385746ea9
Sergey Rudenko <hzzh@outlook.com>
2018-12-11 08:45:33 +0200

    v.1.1

--- WindowsFormsCamers/Form1.cs
+++ WindowsFormsCamers/Form1.cs
@@ -22,10 +22,53 @@ namespace WindowsFormsCamers
             stream.Start();
         }
 
+        void stream_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            //создание стрима на весь экран
+            Bitmap bmp = createBitmap(sender, eventArgs);
+
+            
+            //выделение желтого цвета
+            EuclideanColorFiltering filter = new EuclideanColorFiltering();
+            filter.CenterColor = new RGB(224, 209, 3); //желтый цвет
+            filter.Radius = 100;
+            filter.ApplyInPlace(bmp);
+
+            //Класс считает и извлекает отдельные объекты на изображениях
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.ProcessImage(bmp);
+
+            Rectangle[] rects = blobCounter.GetObjectsRectangles(); //центр обьекта
+
+            foreach (Rectangle recs in rects)
+            {
+                if (rects.Length > 0) //обводить в квадарат
+                {
+                    //DrowPicture(rects, bmp);  // + pictureBox1.Image = bmp;
+
+                    //  OR
+
+                    MoveCoursor(recs.X, recs.Y); // + Form1_Shown -> Application.Exit();
+                }
+           
+               
+               
+            }
+            //pictureBox1.Image = bmp; //рисовать
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
         Bitmap createBitmap(object sender, NewFrameEventArgs eventArgs)
         {
+            int koef = 10;
             Bitmap bmp = (Bitmap)eventArgs.Frame.Clone();
-            bmp = new Bitmap(bmp, new Size(SystemInformation.VirtualScreen.Width/7, SystemInformation.VirtualScreen.Height/7));
+            bmp.RotateFlip(RotateFlipType.RotateNoneFlipX); //зеркально
+            bmp = new Bitmap(bmp, new Size(SystemInformation.VirtualScreen.Width / koef, SystemInformation.VirtualScreen.Height / koef));
             using (bmp)
             {
                 var bmp2 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
@@ -38,53 +81,23 @@ namespace WindowsFormsCamers
             }
         }
 
-
-
-        void stream_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        void DrowPicture(Rectangle[] rectangles, Bitmap bitmap)
         {
-            //создание стрима на весь экран
-            Bitmap bmp = createBitmap(sender, eventArgs);
-           
-            //выделение желтого цвета
-            EuclideanColorFiltering filter = new EuclideanColorFiltering();
-            filter.CenterColor = new RGB(220, 220, 0); //желтый цвет
-            filter.Radius = 120;
-            filter.ApplyInPlace(bmp);
-            bmp.RotateFlip(RotateFlipType.RotateNoneFlipX); //зеркально
-
-            BlobCounter blobCounter = new BlobCounter();
-            //получение координат
-            blobCounter.MinWidth = 15;
-            blobCounter.MinHeight = 15;
-            blobCounter.FilterBlobs = true;
-            blobCounter.ProcessImage(bmp);
-            Rectangle[] rects = blobCounter.GetObjectsRectangles();
-            foreach (Rectangle recs in rects)
+            Rectangle objectRect = rectangles[0];
+            Graphics g = Graphics.FromImage(bitmap);
+            using (Pen pen = new Pen(Color.FromArgb(255, 0, 0), 5))
             {
-                if (rects.Length > 0) //обводить в квадарат
-                {
-                    this.Cursor = new Cursor(Cursor.Current.Handle);
-                    Cursor.Position = new System.Drawing.Point(recs.X, recs.Y);
-                    Cursor.Clip = new Rectangle(this.Location, this.Size);
-
-                    //Rectangle objectRect = rects[0];
-                    //Graphics g = Graphics.FromImage(bmp);
-                    //using (Pen pen = new Pen(Color.FromArgb(160, 255, 160), 5))
-                    //{
-                    //   g.DrawRectangle(pen, objectRect);
-                    //}
-                   //g.Dispose();
-                }
-               
-               
-               
+                g.DrawRectangle(pen, objectRect);
             }
-            //pictureBox1.Image = bmp; //рисовать
+            g.Dispose();
         }
 
-        private void Form1_Shown(object sender, EventArgs e)
+        void MoveCoursor(int X, int Y)
         {
-            Application.Exit();
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new System.Drawing.Point(X, Y);
+            Cursor.Clip = new Rectangle(this.Location, this.Size);
         }
+        
     }
 }
